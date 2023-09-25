0319fb4c64a3e72442ba46d77f471c2b31e4ea4e
Jhonathan <guarino90@gmail.com>
2023-09-25 15:52:08 -0300

    Add hover for the Button component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -5,7 +5,13 @@ const Button = styled.button`
   border: 2px solid ${({ theme }) => theme.colors.secondaryColor};
   border-radius: ${({ theme }) => theme.radius.default};
   color: ${({ theme }) => theme.colors.secondaryColor};
+  box-shadow: 0px 3px 0px ${({ theme }) => theme.colors.secondaryColor};
   padding: 10px 22px;
+
+  &:active {
+    transform: translateY(4px);
+    box-shadow: 0px 0px 0px;
+  }
 `;
 
 export { Button };
