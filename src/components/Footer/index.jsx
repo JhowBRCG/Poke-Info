7fbf410b59f4a7e9159cad435927fa4b2e566ca6
Jhonathan <guarino90@gmail.com>
2023-09-29 20:28:24 -0300

    change font-size for mobiles on the footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <p>
         © Created by{" "}
         <Link to="https://github.com/JhowBRCG" target="no_blank">
-          Jhowbrcg
+          jhowbrcg
         </Link>
       </p>
     </StyledFooter>
@@ -24,6 +24,12 @@ const StyledFooter = styled.footer`
 
     > a {
       color: ${({ theme }) => theme.colors.negativeColor};
+    }
+  }
+
+  @media screen and (max-width: 425px) {
+    p,a {
+      font-size: 1.4rem;
     }
   }
 `;
